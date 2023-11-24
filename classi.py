35b228e7cc6fc8ff4cf2e88606caab2a052863d4
Nicola Pinat <34040525+NickPne7@users.noreply.github.com>
2023-11-24 10:44:59 +0100

    nuovo branch

---
+++ classi.py
@@ -0,0 +1,24 @@
+
+
+class Csvfile():
+    def __init__(self, name):
+        self.name = name
+        try:
+            f = open(self.name, "r")
+        except FileNotFoundError:
+            print("Errore, file non found")
+        
+    def get_data(self):
+        values = []
+        file = open(self.name, "r")
+        for line in file:
+            elements = line.split(",")
+            if elements[0] != "Data":
+                p = [elements[0], elements[1][:-1]]
+                values.append(p)
+        file.close()
+        return values
+    
+
+
+
